9f2d5c95549cf19581e7ba0da4e19b9f6bb38217
Iryna Hromska <irahromska@gmail.com>
2025-09-12 08:51:16 +0300

    fix

--- user/serializers.py
+++ user/serializers.py
@@ -1,6 +1,6 @@
 from typing import Any
 
-from django.contrib.auth import get_user_model
+from django.contrib.auth import get_user_model, authenticate
 from rest_framework import serializers
 
 
@@ -9,9 +9,7 @@ class UserSerializer(serializers.ModelSerializer):
         model = get_user_model()
         fields = ("id", "username", "email", "password", "is_staff")
         read_only_fields = ("id", "is_staff")
-        extra_kwargs = {
-            "password": {"write_only": True, "min_length": 5}
-        }
+        extra_kwargs = {"password": {"write_only": True, "min_length": 5}}
 
     def create(self, validated_data: dict[str, Any]):
         return get_user_model().objects.create_user(**validated_data)
@@ -25,3 +23,27 @@ class UserSerializer(serializers.ModelSerializer):
             user.save()
 
         return user
+
+
+class LoginSerializer(serializers.Serializer):
+    username = serializers.CharField()
+    password = serializers.CharField(write_only=True)
+
+    def validate(self, attrs: dict[str, Any]) -> dict[str, Any]:
+        username = attrs.get("username")
+        password = attrs.get("password")
+
+        user = authenticate(username=username, password=password)
+
+        if not user:
+            raise serializers.ValidationError(
+                {"non_field_errors": "Unable to authenticate with provided credentials."}
+            )
+
+        if not user.is_active:
+            raise serializers.ValidationError(
+                {"non_field_errors": "User account is disabled."}
+            )
+
+        attrs["user"] = user
+        return attrs
