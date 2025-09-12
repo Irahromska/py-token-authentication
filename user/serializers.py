6962c7e3d2e08d4e8dcfcd8504a36899765dcfe0
Iryna Hromska <irahromska@gmail.com>
2025-09-12 08:53:05 +0300

    fix flake8

--- user/serializers.py
+++ user/serializers.py
@@ -36,14 +36,16 @@ class LoginSerializer(serializers.Serializer):
         user = authenticate(username=username, password=password)
 
         if not user:
-            raise serializers.ValidationError(
-                {"non_field_errors": "Unable to authenticate with provided credentials."}
-            )
+            raise serializers.ValidationError({
+                "non_field_errors": (
+                    "Unable to authenticate with provided credentials."
+                )
+            })
 
         if not user.is_active:
-            raise serializers.ValidationError(
-                {"non_field_errors": "User account is disabled."}
-            )
+            raise serializers.ValidationError({
+                "non_field_errors": "User account is disabled."
+            })
 
         attrs["user"] = user
         return attrs
